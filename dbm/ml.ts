0762073f8e7cbc924e04c3013ea98fbcfe478ed7
azelynn-ritua <azelynnrituainbusiness@gmail.com>
2025-03-10 15:44:43 +0800

    move ml interfaces to dbm

---
+++ dbm/ml.ts
@@ -0,0 +1,62 @@
+import { Prisma } from "@prisma/client";
+import { z } from "zod";
+import { incidentInclude } from "./incident";
+
+
+interface ClusteringSettings {
+    componentCount: number | null;
+    clusterCountStart: number;
+    clusterCountEnd: number | null;
+}
+export const clusteringSettingsSchema: z.ZodType<ClusteringSettings> = z.object({
+    componentCount: z.number().nullable(),
+    clusterCountStart: z.number(),
+    clusterCountEnd: z.number()
+});
+
+
+export const feature_count = 2;
+interface IncidentClusteringData {
+    id: number;
+    locationLatitude: number;
+    locationLongitude: number;
+}
+
+
+export interface ClusteringRequest {
+    settings: ClusteringSettings;
+    data: IncidentClusteringData[];
+}
+
+
+interface ClusterResult {
+    clusterCount: number;
+    labels: number[][];
+    score: number;
+}
+const clusterResultSchema: z.ZodType<ClusterResult> = z.object({
+    clusterCount: z.number(),
+    labels: z.array(z.array(z.number())),
+    score: z.number()
+});
+
+
+interface ClusteringResponse {
+    clusterResults: ClusterResult[];
+    optimalClusterCount: number;
+}
+export const clusteringResponseSchema: z.ZodType<ClusteringResponse> = z.object({
+    clusterResults: z.array(clusterResultSchema),
+    optimalClusterCount: z.number()
+});
+
+
+export interface FinalResult {
+    incidents: Prisma.IncidentGetPayload<{ include: typeof incidentInclude }>[];
+    clusterResults: {
+        clusterCount: number;
+        labels: number[][];
+        score: number;
+    }[];
+    optimalClusterCount: number;
+}
